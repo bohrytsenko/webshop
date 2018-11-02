efb377a3080be565558b84043ae91d25a8390d4c
bohrytsenko <hrytsenkobohdan@gmail.com>
2018-11-02 11:45:42 +0100

    slider fix

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -4,24 +4,27 @@ $( document ).ready(function() {
 
     var galleryTop = new Swiper('.gallery-top', {
         spaceBetween: 10,
-        loop: true,
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
+        loop: true,
+        loopedSlides: 4,
         breakpoints: {
             768: {
                 loopedSlides: 3
             }
         }
     });
+
     var galleryThumbs = new Swiper('.gallery-thumbs', {
-        slidesPerView: 3,
-        spaceBetween: 20,
+        spaceBetween: 10,
         centeredSlides: true,
+        slidesPerView: 'auto',
         touchRatio: 0.2,
         slideToClickedSlide: true,
         loop: true,
+        loopedSlides: 4,
         breakpoints: {
             768: {
                 loopedSlides: 3
